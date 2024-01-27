dab7ecb4582cb6521922009a959c0a1cd22fbf41
RANJAN <141898565+mranja@users.noreply.github.com>
2024-01-27 10:14:33 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
 import './App.css';
-import LikeImage from './components/LikeImage';
+import React from 'react';
 import LikePost from './components/LikePost';
+import LikeImage from './components/LikeImage';
+import HigherOrderComponent from './components/HigherOrderComponent/HigherOrderComponent';
+import LikeImageHoc from './components/HigherOrderComponent/LikeimageHOC';
+import LikePostHoc from './components/HigherOrderComponent/LikePostHOC';
+import RenderPropsComponent from './components/RenderProps/RenderPropsComponent';
+import LikeImageRender from './components/RenderProps/LikeImageRender';
+import LikePostRender from './components/RenderProps/LikePostRender';
+
+
+const LikeImageH = HigherOrderComponent(LikeImageHoc)
+const LikePostH = HigherOrderComponent(LikePostHoc)
+
 
 function App() {
   return (
     <div>
       <h3>Some Blog</h3>
-      <div className='buttons'>
-        <LikePost/>
-        <LikeImage/>
+
+
+      <div className="render">
+        <RenderPropsComponent render={(count, increaseCount)=>(
+          <LikeImageRender count={count} increaseCount={increaseCount}/>
+        )} />
+         <RenderPropsComponent render={(count, increaseCount)=>(
+          <LikePostRender count={count} increaseCount={increaseCount} />
+        )} />
       </div>
+
+
+      <div className='buttons'>
+        <LikePost />
+        <LikeImage />
+      </div>
+
+      <div className="hoc">
+        <LikeImageH />
+        <LikePostH />
+      </div>
+
+
     </div>
   );
 }
